5a97590a58132c90a7678a208fcb9665e0e7dbe9
kmhenault <kourtniehenault@gmail.com>
2025-09-30 11:55:27 -0500

    refactor

--- CheckerTest.cs
+++ CheckerTest.cs
@@ -5,9 +5,14 @@ namespace Checker.Tests;
 public class CheckerTests
 {
     [Fact]
-    public void NotOkWhenAnyVitalIsOffRange()
+    public void NotOkWhenAnyVitalIsOutOfRange()
     {
         Assert.False(Checker.VitalsOk(99f, 102, 70));
+    }
+
+    [Fact]
+    public void OkWhenAllVitalsWithinRange()
+    {
         Assert.True(Checker.VitalsOk(98.1f, 70, 98));
     }
 
